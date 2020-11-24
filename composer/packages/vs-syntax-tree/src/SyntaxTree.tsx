1043f754422437f35752e600be73f720ec614b2a
SumayyaZiyad <sumayyaziyad034@gmail.com>
2020-11-24 11:02:09 +0530

    To Checkout - Elkjs

--- composer/packages/vs-syntax-tree/src/SyntaxTree.tsx
+++ composer/packages/vs-syntax-tree/src/SyntaxTree.tsx
@@ -1,19 +1,74 @@
-import * as React from 'react';
+import React from 'react';
+import ELK from 'elkjs/lib/elk-api';
 
 export interface SyntaxTreeProps {
     respTree?: JSON
 }
 
-export class SyntaxTree extends React.Component <SyntaxTreeProps> {
+export interface WriteableProps {
+    respGraph: any
+}
+
+export class SyntaxTree extends React.Component <SyntaxTreeProps, WriteableProps> {
     constructor(props: SyntaxTreeProps){
         super(props);
+
+        this.state = {
+            respGraph: null
+        };
+
+        this.updateValues = this.updateValues.bind(this);
+    }
+
+    updateValues(resp: string){
+        this.setState((state)=>{
+            return {
+                respGraph: resp
+            }
+        })
+    }
+
+    public componentDidMount(){
+        const elk = new ELK({
+            workerUrl: "./elk-worker.min.js"
+        });
+
+        const graph = {
+            id: "root",
+            layoutOptions: { 'elk.algorithm': 'layered' },
+            children: [
+              { id: "n1", width: 30, height: 30 },
+              { id: "n2", width: 30, height: 30 },
+              { id: "n3", width: 30, height: 30 }
+            ],
+            edges: [
+              { id: "e1", sources: [ "n1" ], targets: [ "n2" ] },
+              { id: "e2", sources: [ "n1" ], targets: [ "n3" ] }
+            ]
+        }
+
+        elk.layout(graph)
+            .then((result)=>{
+                this.updateValues(JSON.stringify(result, null, 2));
+            })
+            .catch((e)=>{
+                console.log(e);
+            })
     }
 
     public render (){
+        if (this.state.respGraph){
+            return(
+                <text>
+                    {this.state.respGraph}
+                </text>
+            );
+        }
+
         return (
             <div>
                 <pre>
-                    <text>{JSON.stringify(this.props.respTree, null, 2)}</text>
+                    {JSON.stringify(this.props.respTree, null, 2)}
                 </pre>
             </div>
         );
